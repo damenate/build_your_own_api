9db6d7e6923e8e5a0a26c71aa10ff5c29c4e1a95
Da-Me <damekim1206@gmail.com>
2015-09-25 17:33:25 -0400

    added working class for both apis

--- app/models/condition_gif.rb
+++ app/models/condition_gif.rb
@@ -1,15 +1,17 @@
 class ConditionGif
-  attr_reader :weather, :gif, :response
+  #attr_reader :weather, :gif, :response
   def initialize(sweetgif)
-  #   @weather = CurrentCondition.new(zip)
-  #   @gif = Giphy.new(zip)
-  #   @response = get_response
+    @weather = CurrentCondition.new("zip")
+    @gif = Giphy.new("zip")
+    #@response = get_response
   end
 
   def combo
+    rainy = CurrentCondition.new("zip").condition
+    video = Giphy.new("zip").condition_gif
     case
-    when condition.match(/(rain)/i)
-      return condition_gif
+    when rainy.match(/(rain)/i)
+      return video
     # when @response["current_observation"]["weather"].match(/(sun)/i)
     #   return @response["data"].sample["images"]["fixed_height"]["url"]
     end
